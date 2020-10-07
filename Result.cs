836ce1abb3c883ddf4040cdbf14c25d69ac14aae
210630193 <65148108+210630193@users.noreply.github.com>
2020-10-07 20:59:09 +0800

    Add files via upload

---
+++ Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Result<T>
+    {
+        public int Code = 1;
+        public string msg = "";
+        public T res = default(T);
+        public static Result Ok(T result)
+        {
+            return new Result() { result = result };
+        }
+        public static Result Ok(string message,T Result=default(T))
+        {
+            return new Result() { msg = messgge, res = Result };
+        }
+        public static Result Err(string messgge)
+        {
+            return new Result() { HashCode = -1, msg = messgge };
+        }
+    }
+    public class Result:Result<dynamic>
+    { }
+}
---
+++ UsersController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json(DAL.UserInfo.Instance.GetCount());
+        }
+
+        [HttpPut ]
+        public ActionResult Put([FromBody] Model.UserInfo users)
+        {
+            var result = DAL.UserInfo.Instance.GetModel(username);
+            if (result != null)
+                return Json(Result.Ok(result));
+            else
+                rerurn Json(Result.Err("用户名不存在"));
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody]Model.UserInfo users)
+        {
+            try
+            { 
+               var n = DAL.UserInfo.Instance.Update(users);
+            if (n != 0)
+                return Json(Result.Ok("修改成功"));
+            else
+                return Json(Result.Err("用户名不存在"));
+        }
+            catch (Exception ex)
+            {
+                if (ex.Message.ToLower().Contains("null"))
+                    return Json(Result.Err("用户名、密码、身份不能为空"));
+                else
+                    return Json(Result.Err(ex.Message));
+            }
+        }
+        [HttpPost("check")]
+        public ActionResult Usercheck([FromBody]Model.UserInfo users)
+        {
+            try
+            {
+                var result = DAL.UserInfo.Instance.GetModel(users.userName);
+                if (result == null)
+                    return Json(Result.Err("用户名错误"));
+                else if (result.passWord == users.passWord)
+                {
+                   
+                        result.passWord = "*********";
+                        return Json(Result.Ok("登录成功", result));
+                    }
+                    else
+                        return Json(Rezult.Err("密码错误"));  
+            }
+            catch (Exception ex)
+            {
+                return Json(Result.Err(ex.Message));
+            }
+        }
+        [HttpPost("page")]
+        public ActionResult getPage([FromBody] Model.Page page)
+        {
+            var result = DAL.UserInfo.Instance.GetPage(page);
+            if (result.Count() == 0)
+                return Json(Result.Err("返回记录数为0"));
+            else
+                return Json(Result.0k(result));
+}
+    }
+    }
+}
